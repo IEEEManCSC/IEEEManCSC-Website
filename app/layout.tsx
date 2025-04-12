b194ed734ddcad8654bb3fd94e75ad4461bc408a
Nabil Alex <nabilmohaed46@gmail.com>
2025-04-12 13:07:13 +0200

    base structre

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Cairo } from "next/font/google";
+
+const cairo = Cairo({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "700"],
+});
+
+export const metadata: Metadata = {
+  title: "IEEE",
+  description: "IEEE",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${cairo.className}  antialiased`}>
+        {/* <NavBar />
+        <section className=" pt-36 pb-20 lg:pt-52">{children}</section>
+        <Suspense fallback={<Loading />}>
+        </Suspense>
+        <Footer /> */}
+      </body>
+    </html>
+  );
+}

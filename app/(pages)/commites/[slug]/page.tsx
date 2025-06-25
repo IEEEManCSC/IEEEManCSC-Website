bc5aff53fe1ff8169d40ad2c5f06fcd084ca1411
Nabil Alex <nabilmohaed46@gmail.com>
2025-06-25 03:09:19 +0300

    fix slug error

--- app/(pages)/commites/[slug]/page.tsx
+++ app/(pages)/commites/[slug]/page.tsx
@@ -2,7 +2,11 @@ import { notFound } from "next/navigation";
 import { committeeDetails } from "@/data";
 import CommitteeDetail from "@/app/components/CommitteeDetail";
 
-export default function page({ params }: { params: { slug: string } }) {
+export default async function CommitteePage({
+  params,
+}: {
+  params: { slug: string };
+}) {
   const committee = committeeDetails.find((c) => c.slug === params.slug);
 
   if (!committee) {
